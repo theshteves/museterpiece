774838a677169666203bf13697dd17e6f20a2973
theshteves <stevenkneiser@gmail.com>
2016-03-20 03:50:40 -0400

    Dope Objective-C klutch-mode

--- MuseStatsIos/MuseStatsIos/ViewController.m
+++ MuseStatsIos/MuseStatsIos/ViewController.m
@@ -13,12 +13,17 @@
 
 - (IBAction)press:(UIButton *)sender {
     double val = (double)arc4random() / UINT32_MAX;
-    float muse_val = 0;
+//    float muse_val = 0;
     
     self.theslider.value = val*100;
     
+    NSUInteger size = [self.muse_values count];
+    for (NSUInteger i = 0; i < size; i++) {
+        NSLog(@"%@, ", [self.muse_values objectAtIndex:i]);
+    }
     
 }
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
