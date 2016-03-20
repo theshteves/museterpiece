297d835144a07beea5e687c4de1396892f267963
theshteves <kneisers@msu.edu>
2016-03-20 03:33:42 -0400

    Linked the slider & included appropriate Muse Library

--- MuseStatsIos/MuseStatsIos/ViewController.h
+++ MuseStatsIos/MuseStatsIos/ViewController.h
@@ -4,9 +4,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "../Headers/Muse/IXNMuseDataPacket.h"
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIButton *thebutton;
+@property (weak, nonatomic) IBOutlet UISlider *theslider;
+@property (nonatomic, readonly) NSArray *muse_values;
 
 @end
 
--- MuseStatsIos/MuseStatsIos/ViewController.m
+++ MuseStatsIos/MuseStatsIos/ViewController.m
@@ -12,9 +12,13 @@
 @implementation ViewController
 
 - (IBAction)press:(UIButton *)sender {
+    double val = (double)arc4random() / UINT32_MAX;
+    float muse_val = 0;
+    
+    self.theslider.value = val*100;
+    
     
 }
-
 
 - (void)viewDidLoad {
     [super viewDidLoad];
